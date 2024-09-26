03aa42be2d2e06580c462ac8df528efcc5aed37e
poojaverma02 <poojaverma.ece.aec@gmail.com>
2024-09-26 21:32:20 +0530

    javascipt updated

--- Toggle Key/index.js
+++ Toggle Key/index.js
@@ -1,12 +1,11 @@
 // Add an event listener for when the checkbox (toggle) changes
 document.getElementById('toggle').addEventListener('change', function () {
-    const toggleStatus = document.getElementById('toggle-status');
-    
-    // Check if the checkbox is checked (on state)
-    if (this.checked) {
-      toggleStatus.textContent = 'Switch is ON';
-    } else {
-      toggleStatus.textContent = 'Switch is OFF';
-    }
-  });
+  const toggleStatus = document.getElementById('toggle-status');
   
+  // Check if the checkbox is checked (on state)
+  if (this.checked) {
+    toggleStatus.textContent = 'Switch is ON';
+  } else {
+    toggleStatus.textContent = 'Switch is OFF';
+  }
+});
